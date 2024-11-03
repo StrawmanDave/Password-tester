098d1ad80ea7e9df330532cdecb1f3394d5a6985
unknown <davidgb.strawman@gmail.com>
2024-11-02 18:05:16 -0600

    No less than 8 characters

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// David Barlow 11/2/2024 Homework #27 Validate Password
+
+using System.Diagnostics;
+
+Console.Clear();
+
+Console.WriteLine("The program ran");
+
+Debug.Assert(ValidPassword("Monkey") == false);
+Debug.Assert(ValidPassword("Gorillas") == true);
+
+
+bool ValidPassword(string pw)
+{
+    int characterAmount = 0;
+    for(int i = 0; i<pw.Length; i++)
+    {
+        characterAmount ++;
+    }
+    if(characterAmount < 8)
+    {
+        return false;
+    }else
+    {
+        return true;
+    }
+}
