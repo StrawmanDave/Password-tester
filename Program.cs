5015d73f7ff2a9dd92d31dbf9b39025a6476cce9
unknown <davidgb.strawman@gmail.com>
2024-11-02 19:33:03 -0600

    Must have num and special. Not complete

--- Program.cs
+++ Program.cs
@@ -1,27 +1,49 @@
 ﻿// David Barlow 11/2/2024 Homework #27 Validate Password
 
 using System.Diagnostics;
+using System.Linq.Expressions;
 
 Console.Clear();
 
 Console.WriteLine("The program ran");
 
+// cannot have less than eight charctes
 Debug.Assert(ValidPassword("Monkey") == false);
 Debug.Assert(ValidPassword("Gorillas") == true);
+
+// must have one capital letter
+Debug.Assert(ValidPassword("gorillas") == false);
+Debug.Assert(ValidPassword("Gorillas") == true);
+
+// must not have an asterisk in it\
+Debug.Assert(ValidPassword("Gorillas*") == false);
+Debug.Assert(ValidPassword("*Gorillas") == false);
+
+// must have number
+Debug.Assert(ValidPassword("Gorillas") == false);
+Debug.Assert(ValidPassword("Gorillas4") == true);
+
+// must have on special character that is not a asterisk
+Debug.Assert(ValidPassword("Gorill@s*") == false);
+Debug.Assert(ValidPassword("Gorill@s") == true);
 
 
 bool ValidPassword(string pw)
 {
+    string specialCh = @"!@#$%^&()<>?.,{}[]`~=+-/|_" + "\"";
+    char[] ChSpecial = specialCh.ToCharArray();
     int characterAmount = 0;
-    for(int i = 0; i<pw.Length; i++)
+    int i = 0;
+    for(i = 0; i<pw.Length; i++)
     {
         characterAmount ++;
     }
-    if(characterAmount < 8)
-    {
-        return false;
-    }else
+
+    if(characterAmount >= 8 && pw.Any(char.IsUpper) && !pw.Contains("*"))
     {
         return true;
+    }else
+    {
+        return false;
     }
 }
--- obj/Debug/net8.0/Password-tester.AssemblyInfo.cs
+++ obj/Debug/net8.0/Password-tester.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Password-tester")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0ee1349576ebf563d26c5a94d9089a49f9cc54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098d1ad80ea7e9df330532cdecb1f3394d5a6985")]
 [assembly: System.Reflection.AssemblyProductAttribute("Password-tester")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Password-tester")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
